f26f0402628234aef3a0a89404586f41d7ea9d77
Brian Egan <egan@soundcloud.com>
2016-02-02 00:50:46 +0100

    CombineReducers now takes a wildcard state. Useful for Reducer Composition

--- bansa/src/main/kotlin/com/brianegan/bansa/combineReducers.kt
+++ bansa/src/main/kotlin/com/brianegan/bansa/combineReducers.kt
@@ -2,7 +2,7 @@ package com.brianegan.bansa
 
 import java.util.*
 
-fun <A : Action, S : State> combineReducers(vararg reducers: (S, A) -> S): (S, A) -> S =
+fun <A : Action, S> combineReducers(vararg reducers: (S, A) -> S): (S, A) -> S =
         { state: S, action: A ->
             Arrays.asList(*reducers).fold(state, { state, reducer -> reducer(state, action) })
         }
