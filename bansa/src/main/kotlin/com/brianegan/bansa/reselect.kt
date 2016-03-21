43f7b265b4c1d797580120813e01c15af0e9751e
beyondeye <dario-e@beyond-eye.com>
2016-03-21 12:06:06 +0200

    first version for reselect. state selector library ported from https://github.com/reactjs/reselect (see also http://redux.js.org/docs/recipes/ComputingDerivedData.html)

---
+++ bansa/src/main/kotlin/com/brianegan/bansa/reselect.kt
@@ -0,0 +1,208 @@
+package com.brianegan.bansa
+
+/**
+ * A rewrite for kotlin of https://github.com/reactjs/reselect library for redux (https://github.com/reactjs/redux)
+ * see also "Computing Derived Data" in redux documentation http://redux.js.org/docs/recipes/ComputingDerivedData.html
+ * Created by Dario Elyasy  on 3/18/2016.
+ */
+private val defaultEqualityCheck = { a: Any, b: Any -> a === b }
+
+interface memoizer<T> {
+    fun memoize(vararg inputArgs: Any): T
+}
+
+inline fun <T : Any> Array<out T>.every(transform: (Int, T) -> Boolean): Boolean {
+    forEachIndexed { i, t -> if (!transform(i, t)) return false }
+    return true
+}
+
+// {a:Any,b:Any -> a===b}
+fun <T> defaultMemoize(func: (Array<out Any>) -> T, equalityCheck: (a: Any, b: Any) -> Boolean = defaultEqualityCheck) = object : memoizer<T> {
+    var lastArgs: Array<out Any>? = null
+    var lastResult: T? = null
+    override fun memoize(vararg inputArgs: Any): T {
+        if (lastArgs != null &&
+                lastArgs!!.size == inputArgs.size && inputArgs.every { index, value -> equalityCheck(value, lastArgs!![index]) }) {
+            return lastResult!!
+        }
+        lastArgs = inputArgs
+        lastResult = func(inputArgs)
+        return lastResult!!
+    }
+}
+
+@Suppress("UNUSED_VARIABLE") //S type parameter  marked as unused
+interface SelectorInput<S, I>
+
+/**
+ * a selector function is a function that map a state object to the input for the selector compute function
+ */
+class SelectInput<S, I>(val fn: S.() -> I) : SelectorInput<S, I> {
+}
+
+
+
+/**
+ * note: [Selector] inherit from [SelectorInput] because of support for composite selectors
+ */
+interface Selector<S, O> : SelectorInput<S, O> {
+    operator fun invoke(state: S): O
+    val recomputations: Long
+    fun resetComputations()
+    fun isChanged(): Boolean
+    fun resetChanged()
+}
+
+
+/**
+ * abstract base class for all selectors
+ */
+abstract class AbstractSelector<S, O> : Selector<S, O> {
+    protected var _recomputations = 0L
+    protected var _lastchanged_recomputations = 0L
+    override val recomputations: Long get() = _recomputations
+    override fun resetComputations() {
+        _recomputations = 0
+        _lastchanged_recomputations = 0
+    }
+
+    override fun isChanged(): Boolean = _recomputations != _lastchanged_recomputations
+    override fun resetChanged() {
+        _lastchanged_recomputations = _recomputations
+    }
+
+    protected abstract val computeandcount: (i: Array<out Any>) -> O
+    /**
+     * 'lazy' because computeandcount is abstract. Cannot reference to it before it is initialized in concrete selectors
+     * 'open' because we can provide a custom memoizer if needed
+     */
+    open val memoizer by lazy { defaultMemoize(computeandcount) }  //
+
+}
+
+/**
+ * wrapper class for Selector factory methods [create], that basically is used only to capture
+ * type information for the state parameter
+ */
+class SelectorFor<S> {
+    private fun<S, I> getInputForComputeFun(s: SelectorInput<S, I>, state: S): I =
+            when (s) {
+                is Selector -> s(state)
+                is SelectInput -> s.fn(state)
+                else -> throw NotImplementedError()
+            }
+
+    /**
+     * create a a selector with a single input
+     */
+    fun <I : Any, O> create(si: SelectorInput<S, I>, computefn: (I) -> O) = object : AbstractSelector<S, O>() {
+        override val computeandcount = fun(i: Array<out Any>): O {
+            ++_recomputations
+            @Suppress("UNCHECKED_CAST")
+            return computefn(i[0] as I)
+        }
+
+        override operator fun invoke(state: S): O {
+            return memoizer.memoize(
+                    getInputForComputeFun(si, state)
+            )
+        }
+    }
+
+    /**
+     * create a a selector with a two inputs
+     */
+    fun <I0 : Any, I1 : Any, O> create(
+            si0: SelectorInput<S, I0>,
+            si1: SelectorInput<S, I1>,
+            computeFun: (I0, I1) -> O) = object : AbstractSelector<S, O>() {
+        override val computeandcount = fun(i: Array<out Any>): O {
+            ++_recomputations
+            @Suppress("UNCHECKED_CAST")
+            return computeFun(i[0] as I0, i[1] as I1)
+        }
+
+        override operator fun invoke(state: S): O {
+            return memoizer.memoize(
+                    getInputForComputeFun(si0, state),
+                    getInputForComputeFun(si1, state)
+            )
+        }
+    }
+
+    /**
+     * create a a selector with a 3 inputs
+     */
+    fun <I0 : Any, I1 : Any, I2 : Any, O> create(
+            si0: SelectorInput<S, I0>,
+            si1: SelectorInput<S, I1>,
+            si2: SelectorInput<S, I2>,
+            computefn: (I0, I1, I2) -> O) = object : AbstractSelector<S, O>() {
+        override val computeandcount = fun(i: Array<out Any>): O {
+            ++_recomputations
+            @Suppress("UNCHECKED_CAST")
+            return computefn(i[0] as I0, i[1] as I1, i[2] as I2)
+        }
+
+        override operator fun invoke(state: S): O {
+            return memoizer.memoize(
+                    getInputForComputeFun(si0, state),
+                    getInputForComputeFun(si1, state),
+                    getInputForComputeFun(si2, state)
+            )
+        }
+    }
+
+    /**
+     * create a a selector with a 4 inputs
+     */
+    fun <I0 : Any, I1 : Any, I2 : Any, I3 : Any, O> create(
+            si0: SelectorInput<S, I0>,
+            si1: SelectorInput<S, I1>,
+            si2: SelectorInput<S, I2>,
+            si3: SelectorInput<S, I3>,
+            computefn: (I0, I1, I2, I3) -> O) = object : AbstractSelector<S, O>() {
+        override val computeandcount = fun(i: Array<out Any>): O {
+            ++_recomputations
+            @Suppress("UNCHECKED_CAST")
+            return computefn(i[0] as I0, i[1] as I1, i[2] as I2, i[3] as I3)
+        }
+
+        override operator fun invoke(state: S): O {
+            return memoizer.memoize(
+                    getInputForComputeFun(si0, state),
+                    getInputForComputeFun(si1, state),
+                    getInputForComputeFun(si2, state),
+                    getInputForComputeFun(si3, state)
+            )
+        }
+    }
+
+    /**
+     * create a a selector with a 5 inputs
+     */
+    fun <I0 : Any, I1 : Any, I2 : Any, I3 : Any, I4 : Any, O> create(
+            si0: SelectorInput<S, I0>,
+            si1: SelectorInput<S, I1>,
+            si2: SelectorInput<S, I2>,
+            si3: SelectorInput<S, I3>,
+            si4: SelectorInput<S, I4>,
+            computefn: (I0, I1, I2, I3, I4) -> O) = object : AbstractSelector<S, O>() {
+        override val computeandcount = fun(i: Array<out Any>): O {
+            ++_recomputations
+            @Suppress("UNCHECKED_CAST")
+            return computefn(i[0] as I0, i[1] as I1, i[2] as I2, i[3] as I3, i[4] as I4)
+        }
+
+        override operator fun invoke(state: S): O {
+            return memoizer.memoize(
+                    getInputForComputeFun(si0, state),
+                    getInputForComputeFun(si1, state),
+                    getInputForComputeFun(si2, state),
+                    getInputForComputeFun(si3, state),
+                    getInputForComputeFun(si4, state)
+            )
+        }
+    }
+
+}
---
+++ bansa/src/test/kotlin/com/brianegan/bansa/ReselectTest.kt
@@ -0,0 +1,387 @@
+package com.brianegan.bansa
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
+import org.junit.Test
+/**
+ * Created by Dario on 3/18/2016.
+ * code ported from https://github.com/reactjs/reselect/blob/master/test/test_selector.js
+ * and expanded
+ */
+
+
+
+class ReselectTest {
+    data class StateA(val a:Int)
+    @Test
+    fun basicSelectorTest(){
+        val selector = SelectorFor<StateA>().create(
+                SelectInput { a },
+                {a:Int -> a}
+        )
+        val state = StateA(0)
+        assertThat(selector(state)).isEqualTo(0)
+        assertThat(selector(state)).isEqualTo(0)
+        assertThat(selector.recomputations).isEqualTo(1)
+        assertThat(selector(state.copy(a=1))).isEqualTo(1)
+        assertThat(selector.recomputations).isEqualTo(2)
+    }
+    data class StateAB(val a:Int, val b:Int)
+    @Test
+    fun basicSelectorWithMultipleKeysTest() {
+        val selector= SelectorFor<StateAB>().create(
+                SelectInput {a},
+                SelectInput {b},
+                {a:Int,b:Int -> a+b}
+        )
+        val state1= StateAB(a=1,b=2)
+        assertThat(selector(state1)).isEqualTo(3)
+        assertThat(selector(state1)).isEqualTo(3)
+        assertThat(selector.recomputations).isEqualTo(1)
+        val state2=StateAB(a=3,b=2)
+        assertThat(selector(state2)).isEqualTo(5)
+        assertThat(selector(state2)).isEqualTo(5)
+        assertThat(selector.recomputations).isEqualTo(2)
+    }
+
+    data class StateSubStateA (val sub:StateA)
+    @Test
+    fun MemoizedCompositeArgumentsTest() {
+        val selector = SelectorFor<StateSubStateA>().create(
+                SelectInput {sub},
+                {sub:StateA -> sub}
+        )
+        val state1= StateSubStateA(StateA(1))
+        assertThat(selector(state1)).isEqualTo(StateA(1))
+        assertThat(selector(state1)).isEqualTo(StateA(1))
+        assertThat(selector.recomputations).isEqualTo(1)
+        val state2= StateSubStateA(StateA(2))
+        assertThat(selector(state2)).isEqualTo(StateA(2))
+        assertThat(selector.recomputations).isEqualTo(2)
+    }
+    @Ignore //not implemented
+    @Test
+    fun FirstArgumentCanBeAnArrayTest() {
+        /*
+            const selector = createSelector(
+      [ state => state.a, state => state.b ],
+      (a, b) => {
+        return a + b
+      }
+    )
+    assert.equal(selector({ a: 1, b: 2 }), 3)
+    assert.equal(selector({ a: 1, b: 2 }), 3)
+    assert.equal(selector.recomputations(), 1)
+    assert.equal(selector({ a: 3, b: 2 }), 5)
+    assert.equal(selector.recomputations(), 2)
+         */
+    }
+
+    //TODO I have not really implemented props, but not sure what is the use case.
+    @Test
+    fun CanAcceptPropsTest() {
+
+        val selector = SelectorFor<StateAB>().create(
+                SelectInput {a},
+                SelectInput {b},
+                SelectInput {100},
+                {a:Int,b:Int,c:Int -> a+b+c}
+        )
+        val state1=StateAB(a=1,b=2)
+        assertThat(selector(state1)).isEqualTo(103)
+
+        /*
+       let called = 0
+       const selector = createSelector(
+         state => state.a,
+         state => state.b,
+         (state, props) => props.c,
+         (a, b, c) => {
+           called++
+           return a + b + c
+         }
+       )
+       assert.equal(selector({ a: 1, b: 2 }, { c: 100 }), 103)
+            */
+    }
+
+    @Test
+    fun ChainedSelectorTest() {
+        val selector1= SelectorFor<StateSubStateA>().create(
+                SelectInput {sub},
+                {sub:StateA -> sub}
+        )
+        val selector2= SelectorFor<StateSubStateA>().create(
+                selector1,
+                {sub:StateA -> sub.a }
+        )
+        val state1=StateSubStateA(StateA(1))
+        assertThat(selector2(state1)).isEqualTo(1)
+        assertThat(selector2(state1)).isEqualTo(1)
+        assertThat(selector2.recomputations).isEqualTo(1)
+        val state2=StateSubStateA(StateA(2))
+        assertThat(selector2(state2)).isEqualTo(2)
+        assertThat(selector2.recomputations).isEqualTo(2)
+    }
+    @Ignore //not implemented
+    @Test
+    fun ChainedSelectorWithProps() {
+        /*
+     const selector1 = createSelector(
+      state => state.sub,
+        (state, props) => props.x,
+        (sub, x) => ({ sub, x })
+    )
+    const selector2 = createSelector(
+      selector1,
+      (state, props) => props.y,
+        (param, y) => param.sub.value + param.x + y
+    )
+    const state1 = { sub: {  value: 1 } }
+    assert.equal(selector2(state1, { x: 100, y: 200 }), 301)
+    assert.equal(selector2(state1, { x: 100, y: 200 }), 301)
+    assert.equal(selector2.recomputations(), 1)
+    const state2 = { sub: {  value: 2 } }
+    assert.equal(selector2(state2, { x: 100, y: 201 }), 303)
+    assert.equal(selector2.recomputations(), 2)
+         */
+    }
+    @Ignore //not implemented
+    @Test
+    fun ChainedSelectorWithVariadicArgs() {
+        /*
+    const selector1 = createSelector(
+      state => state.sub,
+        (state, props, another) => props.x + another,
+        (sub, x) => ({ sub, x })
+    )
+    const selector2 = createSelector(
+      selector1,
+      (state, props) => props.y,
+        (param, y) => param.sub.value + param.x + y
+    )
+    const state1 = { sub: {  value: 1 } }
+    assert.equal(selector2(state1, { x: 100, y: 200 }, 100), 401)
+    assert.equal(selector2(state1, { x: 100, y: 200 }, 100), 401)
+    assert.equal(selector2.recomputations(), 1)
+    const state2 = { sub: {  value: 2 } }
+    assert.equal(selector2(state2, { x: 100, y: 201 }, 200), 503)
+    assert.equal(selector2.recomputations(), 2)
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun overrideValueEquals() {
+        /*
+       // a rather absurd equals operation we can verify in tests
+    const createOverridenSelector = createSelectorCreator(
+      defaultMemoize,
+      (a, b) => typeof a === typeof b
+    )
+    const selector = createOverridenSelector(
+      state => state.a,
+        a => a
+    )
+    assert.equal(selector({ a: 1 }), 1)
+    assert.equal(selector({ a: 2 }), 1) // yes, really true
+    assert.equal(selector.recomputations(), 1)
+    assert.equal(selector({ a: 'A' }), 'A')
+    assert.equal(selector.recomputations(), 2)
+
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun customMemoizeTest() {
+        /*
+       const hashFn = (...args) => args.reduce((acc, val) => acc + '-' + JSON.stringify(val))
+    const customSelectorCreator = createSelectorCreator(
+      lodashMemoize,
+      hashFn
+    )
+    const selector = customSelectorCreator(
+      state => state.a,
+      state => state.b,
+      (a, b) => a + b
+    )
+    assert.equal(selector({ a: 1, b: 2 }), 3)
+    assert.equal(selector({ a: 1, b: 2 }), 3)
+    assert.equal(selector.recomputations(), 1)
+    assert.equal(selector({ a: 1, b: 3 }), 4)
+    assert.equal(selector.recomputations(), 2)
+    assert.equal(selector({ a: 1, b: 3 }), 4)
+    assert.equal(selector.recomputations(), 2)
+    assert.equal(selector({ a: 2, b: 3 }), 5)
+    assert.equal(selector.recomputations(), 3)
+    // TODO: Check correct memoize function was called
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun exportedMemoizeTest() {
+        /*
+       let called = 0
+    const memoized = defaultMemoize(state => {
+      called++
+      return state.a
+    })
+
+    const o1 = { a: 1 }
+    const o2 = { a: 2 }
+    assert.equal(memoized(o1), 1)
+    assert.equal(memoized(o1), 1)
+    assert.equal(called, 1)
+    assert.equal(memoized(o2), 2)
+    assert.equal(called, 2)
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun exportedMemoizeWithMultipleArgsTest() {
+        /*
+            const memoized = defaultMemoize((...args) => args.reduce((sum, value) => sum + value, 0))
+    assert.equal(memoized(1, 2), 3)
+    assert.equal(memoized(1), 1)
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun exportedMemoizeWithValueEqualsOverride() {
+        /*
+     // a rather absurd equals operation we can verify in tests
+    let called = 0
+    const valueEquals = (a, b) => typeof a === typeof b
+    const memoized = defaultMemoize(
+      a => {
+        called++
+        return a
+      },
+      valueEquals
+    )
+    assert.equal(memoized(1), 1)
+    assert.equal(memoized(2), 1) // yes, really true
+    assert.equal(called, 1)
+    assert.equal(memoized('A'), 'A')
+    assert.equal(called, 2)
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun structuredSelectorTest() {
+        /*
+      const selector = createStructuredSelector({
+      x: state => state.a,
+      y: state => state.b
+    })
+    const firstResult = selector({ a: 1, b: 2 })
+    assert.deepEqual(firstResult, { x: 1, y: 2 })
+    assert.strictEqual(selector({ a: 1, b: 2 }), firstResult)
+    const secondResult = selector({ a: 2, b: 2 })
+    assert.deepEqual(secondResult, { x: 2, y: 2 })
+    assert.strictEqual(selector({ a: 2, b: 2 }), secondResult)
+         */
+    }
+
+    @Ignore //not implemented
+    @Test
+    fun structuredSelectorWithCustomSelectorCreator() {
+        /*
+       const customSelectorCreator = createSelectorCreator(
+      defaultMemoize,
+      (a, b) => a === b
+    )
+    const selector = createStructuredSelector({
+      x: state => state.a,
+      y: state => state.b
+    }, customSelectorCreator)
+    const firstResult = selector({ a: 1, b: 2 })
+    assert.deepEqual(firstResult, { x: 1, y: 2 })
+    assert.strictEqual(selector({ a: 1, b: 2 }), firstResult)
+    assert.deepEqual(selector({ a: 2, b: 2 }), { x: 2, y: 2 })
+         */
+    }
+    @Test
+    fun resetComputationsTest() {
+        val selector = SelectorFor<StateA>().create(
+                SelectInput { a},
+                {a:Int -> a}
+        )
+        val state1= StateA(a=1)
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector.recomputations).isEqualTo(1)
+        val state2 = StateA(a=2)
+        assertThat(selector(state2)).isEqualTo(2)
+        assertThat(selector.recomputations).isEqualTo(2)
+
+        selector.resetComputations()
+        assertThat(selector.recomputations).isEqualTo(0)
+
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector.recomputations).isEqualTo(1)
+        assertThat(selector(state2)).isEqualTo(2)
+        assertThat(selector.recomputations).isEqualTo(2)
+    }
+    @Test
+    fun isChangedTest() {
+        val selector = SelectorFor<StateA>().create(
+                SelectInput { a},
+                {a:Int -> a}
+        )
+        val state1= StateA(a=1)
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector.isChanged()).isTrue()
+        selector.resetChanged()
+        assertThat(selector(state1)).isEqualTo(1)
+        assertThat(selector.isChanged()).isFalse()
+        val state2=StateA(a=2)
+        assertThat(selector(state2)).isEqualTo(2)
+        assertThat(selector.isChanged()).isTrue()
+    }
+
+    data class State3(val p1:Double, val p2:Double, val p3:Double)
+    @Test
+    fun args3Test() {
+        val selector = SelectorFor<State3>().create(
+                SelectInput { p1},
+                SelectInput { p2},
+                SelectInput { p3},
+                {p1:Double,p2:Double,p3:Double -> p1/p2/p3}
+        )
+        val state= State3(1.0,2.0,3.0)
+        assertThat(selector(state)).isEqualTo(1.0/2.0/3.0)
+    }
+    data class State4(val p1:Double, val p2:Double, val p3:Double,val p4:Double)
+    @Test
+    fun args4Test() {
+        val selector = SelectorFor<State4>().create(
+                SelectInput { p1},
+                SelectInput { p2},
+                SelectInput { p3},
+                SelectInput { p4},
+                {p1:Double,p2:Double,p3:Double,p4:Double -> p1/p2/p3/p4}
+        )
+        val state= State4(1.0,2.0,3.0,4.0)
+        assertThat(selector(state)).isEqualTo(1.0/2.0/3.0/4.0)
+    }
+    data class State5(val p1:Double, val p2:Double, val p3:Double,val p4:Double,val p5:Double)
+    @Test
+    fun args5Test() {
+        val selector = SelectorFor<State5>().create(
+                SelectInput { p1},
+                SelectInput { p2},
+                SelectInput { p3},
+                SelectInput { p4},
+                SelectInput { p5},
+                {p1:Double,p2:Double,p3:Double,p4:Double,p5:Double -> p1/p2/p3/p4/p5}
+        )
+        val state= State5(1.0,2.0,3.0,4.0,5.0)
+        assertThat(selector(state)).isEqualTo(1.0/2.0/3.0/4.0/5.0)
+    }}
+
